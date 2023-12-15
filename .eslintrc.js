40455effd96af910f131bf115f6dd88d7cde0c1e
Suman <sumanpok9@gmail.com>
2023-12-15 23:45:56 +0545

    add eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: latest,
+    sourceType: module,
+    ecmaFeatures: {
+      jsx: true,
+      tsx: true,
+    },
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import './styles.css';
 function App() {
+  const name = 'sdf';
   return <h1>Hello Manually created react app</h1>;
 }
 
 export default App;
+
+// ? TODO
+
+// husky
+// add env for development and production on webpack so that different presets are used.
+// LINT : so that when pushing everything is saved
+// add prettier
